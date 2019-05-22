3af58c6d0165468ecf679ae6d1ecf1b8473952a2
“Paul <prowori2@gmail.com>
2019-05-22 20:13:21 +0300

    Add Admin_View_One page

---
+++ UI-templates/js/landing.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#serve").click(function() {
+    window.open("UI-templates/User_View_All.html", "_self");
+  });
+
+  $("#order").click(function() {
+    window.open("userSide/user.html", "_self");
+  });
+});
