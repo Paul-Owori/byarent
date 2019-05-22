0e851a71be1a0f37f9085b9544c623096b85947d
“Paul <prowori2@gmail.com>
2019-05-22 20:35:28 +0300

    Add Navbar links to User Signup page

--- UI-templates/js/landing.js
+++ UI-templates/js/landing.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
   $("#viewUnits").click(function() {
-    window.open(
-      "paul-owori.github.io/byarent/UI-templates/User_View_All.html",
-      "_self"
-    );
+    window.open("UI-templates/User_View_All.html", "_self");
   });
 
   $("#userSignUp").click(function() {
-    window.open(
-      "paul-owori.github.io/byarent/UI-templates/User_SIgnUp_In.html",
-      "_self"
-    );
+    window.open("UI-templates/User_SIgnUp_In.html", "_self");
   });
 
   $("#houses").click(function() {
