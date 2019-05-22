e8184f6d26c1838c9d7bc2644f6fad70ba086ef2
“Paul <prowori2@gmail.com>
2019-05-22 20:19:34 +0300

    Add page to page connectivity to landing page

--- UI-templates/js/landing.js
+++ UI-templates/js/landing.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   $("#viewUnits").click(function() {
-    window.open("UI-templates/User_View_All.html", "_self");
+    window.open(
+      "paul-owori.github.io/byarent/UI-templates/User_View_All.html",
+      "_self"
+    );
   });
 
   $("#userSignUp").click(function() {
-    window.open("UI-templates/User_SIgnUp_In.html", "_self");
+    window.open(
+      "paul-owori.github.io/byarent/UI-templates/User_SIgnUp_In.html",
+      "_self"
+    );
   });
 });
