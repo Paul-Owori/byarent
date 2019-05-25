edc818c203f6f6d4769928ff21226fc0fdf6b3ec
“Paul <prowori2@gmail.com>
2019-05-25 11:56:36 +0300

    Add AdminSignIn component to client

--- client/src/Components/AdminSignIn.js
+++ client/src/Components/AdminSignIn.js
@@ -20,6 +20,40 @@ class AdminSignIn extends Component {
             Are you an Administrator?
           </h1>
         </p>
+        <Container className="text-center">
+          <Row className="justify-content-center ">
+            <Col md="4" className="px-lg-5 User">
+              <h3 class="font-weight-bold colorME text-center my-3">Sign-In</h3>
+              <Form className="text-center">
+                <FormGroup>
+                  <Input
+                    type="email"
+                    className="form-control"
+                    id="adminSignInEmail"
+                    placeholder="Email"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    type="password"
+                    className="form-control"
+                    id="adminSignInPassword"
+                    placeholder="Password"
+                  />
+                </FormGroup>
+                <Button
+                  id="adminSignIn"
+                  type="submit"
+                  block
+                  color="light"
+                  className="mt-5 mb-3 "
+                >
+                  SignIn
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
       </React.Fragment>
     );
   }
